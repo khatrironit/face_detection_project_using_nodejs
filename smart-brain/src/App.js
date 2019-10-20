9075b7c608bed5d02965edaccd87264c84befbfb
Ronit Khatri <ronitkhatri304@gamil.com>
2019-10-20 22:22:28 +0530

    added logo component

--- smart-brain/src/App.js
+++ smart-brain/src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 //import logo from './logo.svg';
 import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Navigation />
-      {/* {<Logo />
-      <ImageLinkForm />
+      <Logo />
+      {/*<ImageLinkForm />
       <FaceDetection />} */}
     </div>
   );
