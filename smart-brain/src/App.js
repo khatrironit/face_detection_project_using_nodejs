457754d6f2e89734fb1f861ddbf55622b61ac884
Ronit Khatri <ronitkhatri304@gamil.com>
2019-10-20 21:50:44 +0530

    added navigation component

---
+++ smart-brain/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+//import logo from './logo.svg';
+import Navigation from './components/Navigation/Navigation';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation />
+      {/* {<Logo />
+      <ImageLinkForm />
+      <FaceDetection />} */}
+    </div>
+  );
+}
+
+export default App;
