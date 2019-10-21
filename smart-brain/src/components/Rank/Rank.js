1d51b26b8007644416f33e0c240c195be30f3909
Ronit Khatri <ronitkhatri304@gamil.com>
2019-10-21 14:32:15 +0530

    added Rank component

---
+++ smart-brain/src/components/Rank/Rank.js
@@ -0,0 +1,17 @@
+import React from 'react';
+//import './ImageLinkForm.css';
+
+function Rank(){
+    return (
+        <div>
+            <div className = 'f3'>
+                {'Hey Ronit,Your rank is '}
+            </div>
+            <div className = 'f1'>
+                {'#5'}
+            </div>
+        </div>
+    );
+}
+
+export default Rank;
